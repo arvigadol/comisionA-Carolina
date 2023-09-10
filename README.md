ec739b8d69abb649596768b3efa6e512c9a6a84f
Carolina <arvigadol@gmail.com>
2023-09-10 11:19:29 -0300

    Se realizó la conexión con la base de datos. Se cambió en puerto de nuestro servidor a 3002

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # comisionA-Carolina
+
+Para desbloquear el puerto 3306 de Xammp-MySQL: ejecutar CMD como administrador -> netstat nao|findstr 0.0:3306 (| = alt124) se obtiene un número de pid de 4 dígitos XXXX -> taskkill /pid XXXX /f (listo)
+
--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import { posteoRouter } from './src/routes/posteo.routes.js';
+import { startDB } from './src/config/database.js';
 
 const app = express();
 
-const port = 3000;
+const port = 3002;
 
 app.use('/', posteoRouter)
 
+// app.get('/', (req, res) => {
+//     res.send("Todo listo")
+// })
+
 app.listen(port, () => {
     console.log(`server listening in http://localhost:${port}`)
+    startDB()
 })
