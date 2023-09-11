9da4f4a190060caf8bef6ffc9ef5d02a33a0b9f5
Carolina <arvigadol@gmail.com>
2023-09-11 00:31:20 -0300

    Se terminó de configurar el CRUD, agregando el POST y el PUT. Ya está todo listo para empezar a trabajar con ejs

--- web/script.js
+++ web/script.js
@@ -1,12 +1,14 @@
 const contenedor = document.getElementById('container-row')
 const btnCrear = document.getElementById('btn-new')
 const miModal = new bootstrap.Modal(document.getElementById('miModal'))
-const btnSave = document.getElementById('btn-save')
+const btnSave = document.getElementById('btn-save');
+const form = document.getElementById('formulario');
 // const btnEdit = document.getElementById('btn-edit')
 // const btnDelete = document.getElementById('btn-delete')
 
 let html = ''
 let option = ''
+let idForm = ''
 
 const inputTitle = document.getElementById('inputTitle')
 const inputContent = document.getElementById('inputContent')
@@ -27,18 +29,18 @@ document.addEventListener('click', (event) => {
         const idArticle = article.dataset.id
         
         Swal.fire({
-            title: 'Are you sure?',
-            text: "You won't be able to revert this!",
+            title: '¿Estás segurx?',
+            text: "¡No habrá vuelta atrás!",
             icon: 'warning',
             showCancelButton: true,
             confirmButtonColor: '#3085d6',
             cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!'
+            confirmButtonText: '¡Si, eliminar!'
           }).then((result) => {
             if (result.isConfirmed) {
               Swal.fire(
-                'Deleted!',
-                'Your file has been deleted.',
+                '¡Listo!',
+                'Tu publicación fue eliminada',
                 'success'
               )
             }
@@ -72,6 +74,76 @@ document.addEventListener('click', (event) => {
 //         })
 //     }
 // })
+
+document.addEventListener('click', (event) => {
+    if (event.target.matches('#btn-edit')) {
+        const article = event.target.closest('.container-fluid')
+
+        const idArticle = article.dataset.id;
+        const url_imageEdit = article.children[0].children[0].src;
+        const titleEdit = article.children[0].children[1].children[0].textContent;
+        const contentEdit = article.children[0].children[1].children[1].textContent;
+        
+        idForm = idArticle;
+        inputTitle.value = titleEdit;
+        inputContent.value = contentEdit;
+        inputUrl_image.value = url_imageEdit;        
+        option = 'edit';
+        btnSave.textContent = 'Editar';
+        miModal.show();
+    }
+});
+
+form.addEventListener('submit',(event) => {
+    event.preventDefault();
+
+    if (option === "new") {
+        const newPosteo = {
+            title: inputTitle.value,
+            content: inputContent.value,
+            url_image: inputUrl_image.value,
+        };
+        fetch ('http://localhost:3000/api/posteos', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newPosteo),
+        }).then((res) => {
+            if (res.ok) {
+                alert("Tarea creada con éxito");
+                miModal.hide();
+                location.reload();
+            }
+        }).catch(error => {
+            console.error(error);
+        })
+       }
+
+    if (option === "edit") {
+        const newPosteo = {
+            title: inputTitle.value,
+            content: inputContent.value,
+            url_image: inputUrl_image.value,
+    };
+    fetch(`http://localhost:3000/api/posteos/${idForm}`, {
+        method: 'PUT',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newPosteo),
+      }).then((res) => {
+        if (res.ok) {
+            alert("Tarea modificada con éxito");
+            miModal.hide();
+            location.reload();
+        }
+        }).catch(error => {
+            console.error(error);
+        })
+    }
+});
+
 
 fetch('http://localhost:3000/api/posteos')
     .then(res => res.json())
